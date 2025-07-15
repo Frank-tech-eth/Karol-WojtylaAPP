3eeae07e356223c20cbbe0e4ec25312db3b2956d
Frank-tech-eth <francoventura202@tutamail.com>
2025-07-14 23:41:42 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Karol-WojtylaAPP',
+  webDir: 'www'
+};
+
+export default config;
